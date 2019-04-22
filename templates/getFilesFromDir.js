d6e8b9438f892b4ad9b844e596c7c953e7b87ed4
Adam Alcott <aalcott14@gmail.com>
2019-04-22 17:30:55 -0500

    update getFiles template

--- templates/getFilesFromDir.js
+++ templates/getFilesFromDir.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 
+// only want to return index for widgets and root service/library/intResource names
+const confirmNameForType = (filePath) => {
+  const fileName = path.basename(filePath, path.extname(filePath));
+  return (filePath.includes('/config/widgets/') && fileName === 'index') ||
+    (filePath.includes(`/config/internalResources/${fileName}.js`)) ||
+    (filePath.includes(`/code/libraries/${fileName}`)) ||
+    (filePath.includes(`/code/services/${fileName}`))
+};
+
 module.exports = (dir, fileTypes) => {
   var filesToReturn = [];
   function walkDir(currentPath) {
     var files = fs.readdirSync(currentPath);
     for (var i in files) {
       var curFile = path.join(currentPath, files[i]);      
-      if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) != -1) {
+      if (fs.statSync(curFile).isFile() && fileTypes.indexOf(path.extname(curFile)) > -1 && confirmNameForType(curFile)) {
         filesToReturn.push(curFile);
       } else if (fs.statSync(curFile).isDirectory()) {
       walkDir(curFile);
