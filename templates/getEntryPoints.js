0ab4d2b38217906d30e680a8711cdeb6780fad95
Clark Bynum <cbynum@clearblade.com>
2020-09-14 16:06:05 -0500

    use path.sep for path delimiters

--- templates/getEntryPoints.js
+++ templates/getEntryPoints.js
@@ -1,66 +1,95 @@
-const fs = require('fs');
-const error = require('./error');
-const getFilesFromDir = require('./getFilesFromDir');
-const { getLibrariesPath, getServicesPath, getWidgetsPath, getPortalsPath, getWidgetPath, getPortalConfigPath, getAllPath } = require('./getAssets');
-const { allFileTypes, portalTypes, codeTypes } = require('./configConsts');
-
-const createEntryObject = (path, allowedTypes, parentDir) => {
-  const allFiles = getFilesFromDir(path, allowedTypes);
-  return allFiles.reduce((allEntries, srcName) => {
-    const resultName = `${srcName.slice(srcName.indexOf(parentDir)+parentDir.length)}`.replace(/\.tsx|\.jsx|\.ts/gi, '.js');
-    allEntries[resultName] = srcName;
-    return allEntries;
-  }, {});
-}
+const fs = require("fs");
+const error = require("./error");
+const {
+  getLibrariesPath,
+  getServicesPath,
+  getWidgetsPath,
+  getPortalsPath,
+  getWidgetPath,
+  getPortalConfigPath,
+  getAllPath,
+} = require("./getAssets");
+const { allFileTypes, portalTypes, codeTypes } = require("./configConsts");
+const createEntryObject = require("./createEntryObject");
+const path = require("path");
 
 module.exports = {
   getAllEntries: () => {
     const allPath = getAllPath(true);
     if (fs.existsSync(allPath)) {
-      return createEntryObject(allPath, allFileTypes, 'src/');
+      return createEntryObject(allPath, allFileTypes, `src${path.sep}`);
     }
-    error(`Missing src path: ${allPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${allPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getAllPortalsEntries: () => {
     const portalsPath = getPortalsPath(true);
     if (fs.existsSync(portalsPath)) {
-      return createEntryObject(portalsPath, portalTypes, 'portals/');
+      return createEntryObject(portalsPath, portalTypes, `portals${path.sep}`);
     }
-    error(`Missing src path: ${portalsPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${portalsPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getPortalEntries: (portal) => {
     const configPath = getPortalConfigPath(portal, true);
     if (fs.existsSync(configPath)) {
-      return createEntryObject(configPath, portalTypes, 'config/');
+      return createEntryObject(configPath, portalTypes, `config${path.sep}`);
     }
-    error(`Missing src path: ${configPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${configPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getAllWidgetsEntries: (portal) => {
     const widgetsPath = getWidgetsPath(portal, true);
     if (fs.existsSync(widgetsPath)) {
-      return createEntryObject(widgetsPath, portalTypes, 'widgets/');
+      return createEntryObject(widgetsPath, portalTypes, `widgets${path.sep}`);
     }
-    error(`Missing src path: ${widgetsPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${widgetsPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getWidgetEntries: (portal, widgetId) => {
     if (widgetId) {
       const widgetPath = getWidgetPath(portal, widgetId, true);
-      return createEntryObject(widgetPath, portalTypes, `${widgetId}/`);
+      return createEntryObject(
+        widgetPath,
+        portalTypes,
+        `${widgetId}${path.sep}`
+      );
     }
-    error(`Missing src path for widget with id ${module.exports.widgetId}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true);
+    error(
+      `Missing src path for widget with id ${module.exports.widgetId}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getAllServicesEntries: () => {
     const servicesPath = getServicesPath(true);
     if (fs.existsSync(servicesPath)) {
-      return createEntryObject(servicesPath, codeTypes, 'services/');
+      return createEntryObject(servicesPath, codeTypes, `services${path.sep}`);
     }
-    error(`Missing src path: ${servicesPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${servicesPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
   getAllLibrariesEntries: () => {
     const librariesPath = getLibrariesPath(true);
     if (fs.existsSync(librariesPath)) {
-      return createEntryObject(librariesPath, codeTypes, 'libraries/');
+      return createEntryObject(
+        librariesPath,
+        codeTypes,
+        `libraries${path.sep}`
+      );
     }
-    error(`Missing src path: ${librariesPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`, true); 
+    error(
+      `Missing src path: ${librariesPath}. To resolve this issue, create a 'src' directory in the root directory of this system and use cb-dev-kit create or cb-dev-kit generate to add files to it.`,
+      true
+    );
   },
-}
+};
